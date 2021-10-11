1c3f7861d887a60656019e783424e07a3153f49b
salah856 <salah_elhossiny856@yahoo.com>
2021-10-11 16:43:42 +0200

    edit the connectionstring for mongodb connection

--- Server/EpicoreNodeServer/MongoService/mongoConnection.js
+++ Server/EpicoreNodeServer/MongoService/mongoConnection.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 class MongoConnection{
 
     constructor(){
-
+        this.connectionString = `mongodb+srv://Allah012:${process.env.MONGO_PASSWORD}@cluster0.lum93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
     }
     
     connect(app){
         
-        mongoose.connect(`mongodb+srv://Allah012:${process.env.MONGO_PASSWORD}@cluster0.lum93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+        mongoose.connect(this.connectionString) 
         .then(() => {
             app.listen(5000); 
         }).catch(err => {
