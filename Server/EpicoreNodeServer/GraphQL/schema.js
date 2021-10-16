889e99b6403f1c824b4d8ad1a06591831e788ef0
salah856 <salah_elhossiny856@yahoo.com>
2021-10-16 13:27:12 +0200

    refactor the schema of graphql file structure in server

--- Server/EpicoreNodeServer/GraphQL/schema.js
+++ Server/EpicoreNodeServer/GraphQL/schema.js
@@ -2,58 +2,14 @@ const {PubSub} = require('apollo-server');
 const pubSub = new PubSub();
 const {CouponService} = require('../CouponService/couponService');
 const {makeExecutableSchema} = require('@graphql-tools/schema');
+const {typeDefs} = require('./typeDefs'); 
+
 
 const { 
   createCoupon, 
   redeemCouponByCode,
   requestCouponByFoodItemName 
 } = new CouponService();
-
-const typeDefs = `
-
-    input CouponInput {
-      text: String
-      code: Int
-      expiryDate: String
-    }
-
-    input Code {
-      code: Int
-    }
-
-    type Merchant {
-      _id: ID
-      merchantName: String
-      coupons: [Coupon]
-    }
-
-    type Coupon {
-      _id: ID
-      text: String
-      code: Int
-      expiryDate: String
-      merchant: Merchant
-      userID: String
-      requested: Boolean
-      redeemed: Boolean
-    }
-
-    type Query {
-      getCouponByFoodItem(foodItemName: String): Coupon
-    }
-
-
-    type Mutation {
-      createCoupon(coupon: CouponInput!): Coupon
-      redeemCoupon(code: Int): Coupon
-    }
-
-    type Subscription {
-      couponRedeemed(code: Code): Coupon
-    }
-    ,
-
-`; 
 
 
 const resolvers = {
---
+++ Server/EpicoreNodeServer/GraphQL/typeDefs.js
@@ -0,0 +1,48 @@
+
+const typeDefs = `
+
+    input CouponInput {
+      text: String
+      code: Int
+      expiryDate: String
+    }
+
+    input Code {
+      code: Int
+    }
+
+    type Merchant {
+      _id: ID
+      merchantName: String
+      coupons: [Coupon]
+    }
+
+    type Coupon {
+      _id: ID
+      text: String
+      code: Int
+      expiryDate: String
+      merchant: Merchant
+      userID: String
+      requested: Boolean
+      redeemed: Boolean
+    }
+
+    type Query {
+      getCouponByFoodItem(foodItemName: String): Coupon
+    }
+
+
+    type Mutation {
+      createCoupon(coupon: CouponInput!): Coupon
+      redeemCoupon(code: Int): Coupon
+    }
+
+    type Subscription {
+      couponRedeemed(code: Code): Coupon
+    }
+    ,
+
+`; 
+
+module.exports = {typeDefs}; 
